646a122396fb8bde475f1fed0bc73507bcbf1c4c
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-01-21 19:02:10 +0300

    This part incorporates adding of new data(username) into the database and creating a personalized greeting for new and returning users. This is after a month of learning the basics of Flask and Flask-SQLAlchemy. It incorporates the use of database in flask view functions.

---
+++ database.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, redirect, url_for, session, flash, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "hard to guess string"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/user', methods=["POST", "GET"])
+def user():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session["known"] = False
+        else:
+            session["known"] = True
+        session["name"] = form.name.data
+        form.name.data = ''
+        return redirect(url_for("user"))
+    return render_template("user.html", form=form, name=session.get("name"), known=session.get("known", False))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
