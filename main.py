6637c1755734b0b9ecff87cf1ae33d753c6c3962
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-01-26 10:47:48 +0300

    Added a thread to help in sending asynchronous email

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail, Message
+from threading import Thread
 import os
 
 # absolute path for the database file
@@ -69,13 +70,22 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+# adding a thread function to send email asynchronously
+# this helps in making the page to not be unresponsive
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 # function that avoids having to create email messages manually every time
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
                     sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 @app.route('/')
