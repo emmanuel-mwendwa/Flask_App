a5105a363b2d071f880d371b21fd242d5a535cfd
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-03-13 10:21:08 +0300

    User Profile Editor

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 # a FlaskForm model/class that helps create a HTML template form
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField('Full name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About Me')
+    submit = SubmitField('Update')
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, current_app
+from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
-from flask import current_app
 from app.email import send_email
 
 @main.route('/')
@@ -35,3 +35,19 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template("user.html", user=user)
+
+@main.route('/edit-profile', methods=["POST", "GET"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        return redirect(url_for('main.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
