f9cdf1d30b94765318cbe5b27cfd5ea1ce303ff4
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-04-10 12:10:08 +0300

    handling rich text on the server

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from flask import current_app, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
+from markdown import markdown
+import bleach
 import datetime
 import jwt
 import hashlib
@@ -241,8 +243,17 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime(), index=True, default=datetime.datetime.utcnow())
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html', tags=allowed_tags, strip=True)))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 class AnonymousUser(AnonymousUserMixin):
