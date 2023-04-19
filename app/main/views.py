012a0af069cfb2733cbcee216afd12f7b7b09548
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-04-19 10:33:11 +0300

    obtaining followed posts, showing all or followed posts, making users their own followers

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, request, redirect, url_for, flash, current_app, abort
+from flask import render_template, request, redirect, url_for, flash, current_app, abort, make_response
 from flask_login import login_required, current_user
 from . import main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
@@ -18,10 +18,18 @@ def index():
         db.session.commit()
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, 
+    show_followed = False
+    if current_user.is_authenticated:
+        show_followed = bool(request.cookies.get('show_followed', ''))
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(page=page, 
                 per_page=current_app.config["FLASKY_POSTS_PER_PAGE"], error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts,
+                           show_followed=show_followed, pagination=pagination)
     
 
 @main.route('/user/<username>', methods=["POST", "GET"])
@@ -157,3 +165,17 @@ def followed_by(username):
     follows = [{'user': item.followed, 'timestamp': item.timestamp}
                for item in pagination.items]
     return render_template('followers.html', user=user, title="Followers of", endpoint="main.followed_by", pagination=pagination, follows=follows)
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('main.index')))
+    resp.set_cookie('show_followed', '', max_age=30*24*60*60)
+    return resp
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('main.index')))
+    resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
+    return resp
--- app/models.py
+++ app/models.py
@@ -137,6 +137,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
             if self.email is not None and self.avatar_hash is None:
                 self.avatar_hash = self.gravatar_hash()
+        
+        self.follow(self)
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
@@ -276,6 +278,19 @@ class User(UserMixin, db.Model):
             return False
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
+    
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+    
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).\
+                filter(Follow.follower_id == self.id)
     
 
 class AnonymousUser(AnonymousUserMixin):
