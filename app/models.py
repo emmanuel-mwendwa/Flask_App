14eeed92a5e74931cfe3b1bd609aa007a2a13aa9
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-04-19 15:01:15 +0300

    comments model and database

--- app/models.py
+++ app/models.py
@@ -120,6 +120,7 @@ class User(UserMixin, db.Model):
                                cascade='all, delete-orphan')
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
 
     # function to locate users last seen time
     def ping(self):
@@ -316,10 +317,12 @@ class Post(db.Model):
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime(), index=True, default=datetime.datetime.utcnow())
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
     
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
-        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3']
         
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html', 
@@ -327,3 +330,24 @@ class Post(db.Model):
 
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.utcnow())
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'strong', 'p']
+        
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html', 
+            tags=allowed_tags, strip=True)))
+        
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
