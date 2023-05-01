ddfbe436f4224b5ece4d5f3e5f855edfa305c68b
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-05-01 14:08:04 +0300

    error handlers with json

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 
@@ -10,10 +10,20 @@ def forbidden(e):
 # custom error handling pages
 @main.app_errorhandler(404)
 def page_not_found(e):
+    if request.accept_mimetypes.acept_json and\
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
     return render_template("404.html"), 404
 
 
 # custom error handling pages
 @main.app_errorhandler(500)
 def internal_server_error(e):
+    if request.accept_mimetypes.acept_json and\
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
     return render_template("500.html"), 500
