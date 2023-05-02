517fa1d0eb3fb92426bddd39480970a668401351
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-05-02 20:47:15 +0300

    Implementing resource endpoints and pagination of large resource collections

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -21,6 +21,17 @@ def verify_password(email_or_token, password):
     g.token_used = False
     return user.verify_password(password)
 
+@auth.error_handler
+def auth_error():
+    return unauthorized('Invalid credentials')
+
+@api.before_request
+@auth.login_required
+def before_request():
+    if not g.current_user.is_anonymous and \
+            not g.current_user.confirmed:
+        return forbidden('Unconfirmed account')
+
 @api.route('/tokens/', methods=["POST"])
 def get_token():
     if g.current_user.is_anonymous or g.token_used:
--- app/api/errors.py
+++ app/api/errors.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from . import api
+from app.exceptions import ValidationError
 
 def bad_request(message):
     response = jsonify({'error': 'bad request', 'message':message})
@@ -15,3 +16,7 @@ def forbidden(message):
     response = jsonify({'error': 'forbidden', 'message':message})
     response.status_code = 403
     return response
+
+@api.errorhandler(ValidationError)
+def validation_error(e):
+    return bad_request(e.args[0])
