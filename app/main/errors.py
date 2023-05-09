c6022fb44ddef9a4e6d18614aca7c0e840ece206
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-05-10 01:06:37 +0300

    added a json to the main/errors.py file

--- app/main/errors.py
+++ app/main/errors.py
@@ -10,7 +10,7 @@ def forbidden(e):
 # custom error handling pages
 @main.app_errorhandler(404)
 def page_not_found(e):
-    if request.accept_mimetypes.acept_json and\
+    if request.accept_mimetypes.accept_json and \
             not request.accept_mimetypes.accept_html:
         response = jsonify({'error': 'not found'})
         response.status_code = 404
@@ -21,7 +21,7 @@ def page_not_found(e):
 # custom error handling pages
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    if request.accept_mimetypes.acept_json and\
+    if request.accept_mimetypes.accept_json and\
             not request.accept_mimetypes.accept_html:
         response = jsonify({'error': 'internal server error'})
         response.status_code = 500
